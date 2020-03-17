68ff966fca8dd185899cdb683679f30e2fd56575
woksel <56549819+woksel@users.noreply.github.com>
2020-03-17 23:27:29 +0400

    feat: change animation function and add new events

--- script.js
+++ script.js
@@ -1,47 +1,102 @@
 window.onload = () => {
-  let animation = (el, css, delay, callback) => {
-    let delta = 10;
-    let s = css.style;
-    let offset = parseInt(el.style[s]) ? parseInt(el.style[s]) : 0;
-    let v = css.value == 0 ? -offset : css.value;
-    let ed = css.type;
-    let speed = v / (delay / delta);
-    let time = Math.abs((v / speed) * delta);
+  let animate = function(cssObj, duration, callback) {
+    let start = performance.now();
 
-    if (!time) return;
+    //linear timing function
+    let timing = t => t;
 
-    let start = 0;
+    //drawing function default
+    let draw = (s, p) => {
+      s.styleObj[s.type] = p * s.newValue + s.prev + s.ed;
+    };
 
-    let anim = setInterval(() => {
-      let current = start * speed + offset;
+    //from css properties object create map structure for convenient use
 
-      el.style[s] = `${current + ed}`;
+    let cssMap = new Map(Object.entries(cssObj));
 
-      start++;
-    }, delta);
-    setTimeout(() => {
-      clearInterval(anim);
-      if (callback) callback();
-    }, time + delta);
+    //for every css properties call animation in for-of cycle
+
+    for (let i of cssMap) {
+      // css property name
+
+      let type = i[0];
+
+      //css property value + property unit, two type
+
+      let value, ed;
+
+      //if typeof value string
+
+      if (typeof i[1] == "string") {
+        value = parseInt(
+          [...i[1]]
+            .filter(n => {
+              if (/[-0-9]/.test(n)) return n;
+            })
+            .join("")
+        );
+
+        //calculate unit if it is string
+
+        ed = [...i[1]]
+          .filter(n => {
+            if (/[%a-z]/.test(n)) return n;
+          })
+          .join("");
+
+        //and if typeof value number
+      } else value = i[1];
+
+      //and if property unit is undefined or empty string
+
+      ed = ed ? ed : "";
+
+      let styleObj;
+
+      //define style object, it was done, because some properties are taken
+      //from property style, but any are taken directly
+
+      if (type === "scrollTop") styleObj = this;
+      else styleObj = this.style;
+
+      //define prevision and new value of the current css property
+
+      let prev = parseInt(styleObj[type]) ? parseInt(styleObj[type]) : 0;
+      let newValue = value - prev;
+
+      //and actually call animation loop, using requestAnimationFrame function
+
+      requestAnimationFrame(function animate(time) {
+        //define time fraction for control the animation process
+
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        //using timing function for function curve
+
+        let progress = timing(timeFraction);
+
+        //call drawing function
+
+        draw({ styleObj, type, prev, newValue, ed }, progress);
+
+        //condition for exiting recursion
+
+        if (timeFraction < 1) requestAnimationFrame(animate);
+
+        //and call callback function in the end
+        if (progress == 1 && callback)
+          requestAnimationFrame(() => {
+            callback();
+          });
+      });
+    }
   };
   let scroll = e => {
-    // let y = e.getBoundingClientRect().top - 95;
-    // let delay = 10;
-    // let t = 200 / delay;
-    // let speed = y / t;
-    // let time = Math.abs((y / speed) * delay);
-
-    // if (y == 0) return;
-
-    // let i = 0;
-    // let a = setInterval(() => {
-    //   window.scrollBy(0, speed);
-    // }, delay);
-    // setTimeout(() => {
-    //   clearInterval(a);
-    // }, time + delay);
-
-    window.scrollBy(0, e.getBoundingClientRect().top - 95);
+    let y = e.offsetTop - 95;
+    animate.call(document.documentElement, { scrollTop: y }, 300, () => {
+      window.addEventListener("scroll", switchOnScroll);
+    });
   };
   let toLink = e => {
     let link = e.getAttribute("data-link");
@@ -58,12 +113,13 @@ window.onload = () => {
 
     element.classList.add("m-active");
 
+    window.removeEventListener("scroll", switchOnScroll);
+
     if (element.hasAttribute("data-link")) toLink(element);
   };
   let sliderControl = e => {
     let t = e.currentTarget;
     let c = e.target;
- 
     if (!c.classList.contains("chev")) return;
 
     if (t.isStarted) {
@@ -99,14 +155,11 @@ window.onload = () => {
         break;
     }
 
-    animation(
-      slider,
-      { style: "left", value: -100 * index, type: "%" },
-      300,
-      () => {
-        delete t.isStarted;
-      }
-    );
+    let cur = -100 * index;
+
+    animate.call(slider, { left: cur + "%" }, 300, () => {
+      delete t.isStarted;
+    });
 
     let newIndex = Math.abs(index);
     img = slider.querySelector(`[index="${newIndex}"]`);
@@ -181,8 +234,11 @@ window.onload = () => {
   };
   let hideWindow = e => {
     let c = e.currentTarget;
+    let w = c.closest(".window");
 
-    c.closest(".window").remove();
+    animate.call(w, { opacity: 0 }, 100, () => {
+      w.remove();
+    });
   };
   let showWindow = (e, n) => {
     let w = document.createElement("div");
@@ -208,6 +264,11 @@ window.onload = () => {
     );
 
     document.querySelector("main").prepend(w);
+
+    w.style.display = "flex";
+
+    animate.call(w, { opacity: 1 }, 100);
+
     document
       .querySelector(".window-hide")
       .addEventListener("click", hideWindow);
@@ -224,6 +285,22 @@ window.onload = () => {
 
     showWindow([subject, desc], "The letter was send");
   };
+  let switchOnScroll = e => {
+    let nav = document.querySelector(".nav-list");
+
+    for (let i of nav.children) {
+      if (
+        document
+          .querySelector("." + i.getAttribute("data-link"))
+          .getBoundingClientRect().top <=
+        0 + 95
+      ) {
+        for (let e of nav.children) e.classList.remove("m-active");
+
+        i.classList.add("m-active");
+      }
+    }
+  };
 
   document.querySelector(".nav-list").addEventListener("click", switchableMenu);
   document
@@ -234,4 +311,5 @@ window.onload = () => {
   document.querySelector(".slider").addEventListener("click", toggleScreen);
   document.querySelector(".content").addEventListener("click", clickableImages);
   document.querySelector("form").addEventListener("submit", btnSubmit);
+  window.addEventListener("scroll", switchOnScroll);
 };
